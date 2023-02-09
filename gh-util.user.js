5a5a5cf6c137fbbfd836c43c99861b8bcb459418
Aolin <aolinz@outlook.com>
2023-02-09 22:30:41 +0800

    script: (CreateFileLink) add a File element to quick access to FIles Changed page
    Signed-off-by: Aolin <aolin.zhang@pingcap.com>
    Signed-off-by: Aolin <aolinz@outlook.com>

---
+++ gh-util.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         Octopus GitHub
+// @version      0.1
+// @description  A userscript for GitHub
+// @author       Oreo
+// @match        https://github.com/*/pulls*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+
+    'use strict';
+
+    function CreateFileLink() {
+
+        // Get all div elements with an id that starts with "issue_"
+        var issueElements = document.querySelectorAll('div[id^="issue_"]');
+        issueElements.forEach((element) => {
+            var issueId = element.getAttribute("id")
+            var originalLinkElement = document.getElementById(issueId + "_link")
+            var originalLink = originalLinkElement.getAttribute("href")
+            var newLink = originalLink + "/files"
+            // Get all span elements within the current element
+            var spanElements = element.querySelectorAll('span[class="opened-by"]');
+            if (spanElements.length == 1) {
+                var openedBy = spanElements[0];
+                var linkSpanElement = document.createElement('span');
+                linkSpanElement.setAttribute('class', 'd-inline-block mr-1')
+                var dotSpanElement = document.createElement('span');
+                dotSpanElement.innerHTML = ' • ';
+                dotSpanElement.setAttribute('class', 'd-inline-block mr-1')
+                var linkElement = document.createElement('a')
+                linkElement.setAttribute('href', newLink)
+                linkElement.setAttribute('class', 'Link--muted')
+                linkElement.innerHTML = "Files"
+                linkSpanElement.appendChild(linkElement)
+                openedBy.insertAdjacentElement('beforebegin', linkSpanElement)
+                openedBy.insertAdjacentElement('beforebegin', dotSpanElement);
+            }
+        })
+
+    }
+
+    CreateFileLink();
+
+})();
