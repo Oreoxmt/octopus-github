f60257878cca663c7f78ab945fdbad262242344e
Aolin <aolinz@outlook.com>
2023-06-30 10:02:30 +0800

    Update gh-util.user.js

--- gh-util.user.js
+++ gh-util.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Octopus GitHub
-// @version      0.9
+// @version      0.10
 // @description  A userscript for GitHub
 // @author       Oreo
 // @homepage     https://github.com/Oreoxmt/octopus-github
